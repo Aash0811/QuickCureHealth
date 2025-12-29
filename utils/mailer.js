986c306ebe59a38a16cc5cb43e54be2e6ad16caa
Aashrith Rao <aashrith.gonti143@gmail.com>
2025-12-29 22:52:40 +0530

    mail recover

--- utils/mailer.js
+++ utils/mailer.js
@@ -2,6 +2,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   service: 'Gmail',
+  host:"smtp.gmail.com",
+  port: 456,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,  
