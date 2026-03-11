afb752663608ac702f6724cf0cc9949320837f37
Aashrith Rao Gonti <aashrith.gonti143@gmail.com>
2026-03-11 16:24:11 +0530

    set mail

--- utils/mailer.js
+++ utils/mailer.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: true,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
